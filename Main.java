27806059f8e32268dcb81dbf13c9de4dc54d6f17
Mikail <mikailsyed@yahoo.com>
2025-01-01 17:34:32 -0500

    Test -- may have messed this up

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String [] args) {
+        System.out.println("Hello. I am the Mikail Syed Trademarked Financial Planner. Tell me an item you want to save up for.");
+
+        Scanner scan = new Scanner(System.in);
+
+        String itemName = scan.nextLine();
+        // System.out.println(itemName);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+Financial planner is a basic app that allows you to set an item you want to save up for, then calculate how much savings you'll need to get that item within 12 months.
+
+Project objective is very trivial, just a tool I'm building to reintroduce myself to the Java programming language.
