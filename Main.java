c21217a72ead4e14a3a036fc62fd38260ebc2126
Mikail <mikailsyed@yahoo.com>
2025-01-01 17:44:55 -0500

    Add extra details

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class Main {
     public static void main(String [] args) {
+
+        ArrayList<String> itemList = new ArrayList<String>();
         System.out.println("Hello. I am the Mikail Syed Trademarked Financial Planner. Tell me an item you want to save up for.");
 
         Scanner scan = new Scanner(System.in);
 
         String itemName = scan.nextLine();
         // System.out.println(itemName);
+        itemList.add(itemName);
+        // Make an Item class that gets the expenses lol
+        scan.close();
+        
     }
 }
