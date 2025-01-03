774c4f01b1c86813b6a56f58b73e6f589834c56d
Mikail <mikailsyed@yahoo.com>
2025-01-03 10:10:42 -0500

    Adding entry feature

--- Main.java
+++ Main.java
@@ -5,7 +5,36 @@ class Main {
     public static void main(String [] args) {
 
         ArrayList<Item> itemList = new ArrayList<Item>();
-        System.out.println("Hello. I am the Mikail Syed Trademarked Financial Planner. Tell me an item you want to save up for.");
+        System.out.println("Hello. I am the Mikail Syed Trademarked Financial Planner.");
+
+        hub(itemList);
+        
+    }
+
+    public static void hub(ArrayList<Item> itemList) {
+        System.out.println("What would you like to do?");
+        System.out.println("1. Enter a new item");
+        System.out.println("2. List current items");
+        Scanner scan = new Scanner(System.in);
+        int option = scan.nextInt();
+        if (option > 2 || option < 1) {
+            System.out.println("Invalid choice. Try again.");
+            hub(itemList);
+            scan.close();
+            return;
+        }
+        if (option == 1) {
+            enterItem(itemList);
+        }
+        if (option == 2) {
+            listItems(itemList);
+        }
+        scan.close();
+    }
+
+    public static void enterItem(ArrayList<Item> itemList) {
+
+        System.out.println(" Tell me an item you want to save up for.");
 
         Scanner scan = new Scanner(System.in);
 
@@ -25,28 +54,11 @@ class Main {
         System.out.print(itemCost);
         System.out.println("");
         scan.close();
-        
+
+        hub(itemList);
     }
 
-    public void hub(ArrayList<Item> itemList) {
-        System.out.println("What would you like to do?");
-        System.out.println("1. Enter a new item");
-        System.out.println("2. List current items");
-        Scanner scan = new Scanner(System.in);
-        int option = scan.nextInt();
-        if (option != 1 || option != 2) {
-            System.out.println("Invalid choice. Try again.");
-            hub(itemList);
-            scan.close();
-            return;
-        }
-        if (option == 2) {
-            listItems(itemList);
-        }
-        scan.close();
-    }
-
-    public void listItems(ArrayList<Item> itemList) {
+    public static void listItems(ArrayList<Item> itemList) {
         for (int i = 0; i < itemList.size(); i++) {
             System.out.println("Item: " + itemList.get(i).getName());
             System.out.println("Cost: ");
